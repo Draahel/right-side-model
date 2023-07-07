731f03c2e6f87cd3b94c5c1baaf53e967bb8d635
Daniel Mercado <dms9812@hotmail.com>
2023-07-06 19:53:04 -0500

    Change bottom to right

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'right-modal-sample';
+  displayingModal = 'none';
+
+  constructor(){}
+
+  ngOnInit(): void {
+
+  }
+
+  displayModal(): void {
+    this.displayingModal = 'block';
+  }
+
+  hideModal(): void {
+    this.displayingModal = 'none'
+  }
+
+  closeModalOutSite(event:any): void {
+    const { id } = event.target;
+    if (id === 'myModal'){
+      this.hideModal();
+    }
+  }
+
 }
---
+++ src/app/side-modal/side-modal.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-side-modal',
+  templateUrl: './side-modal.component.html',
+  styleUrls: ['./side-modal.component.scss']
+})
+export class SideModalComponent implements OnInit {
+
+
+  constructor(){}
+
+  ngOnInit(): void {
+
+  }
+
+}
